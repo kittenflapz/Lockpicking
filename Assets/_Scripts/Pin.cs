6ca12ffb93bfeec157da9dfe142223e16672a5f8
Catt Symonds <catherine.symonds.thompson@gmail.com>
2021-02-10 19:27:28 -0500

    tumblers pins and stuff

---
+++ Assets/_Scripts/Pin.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pin : MonoBehaviour
+{
+    public string keybind;
+    public float fallSpeed;
+    public float maxHeight;
+    public float minHeight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(keybind))
+        {
+            if (transform.position.y < maxHeight)
+            { 
+                transform.position = new Vector2(transform.position.x, transform.position.y + 0.02f); 
+            }
+        }
+        else
+        {
+            if (transform.position.y > minHeight)
+            {
+                transform.position = new Vector2(transform.position.x, transform.position.y - fallSpeed * 0.01f);
+            }
+        }
+    }
+}
---
+++ Assets/_Scripts/PinManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// keeps list of the pins currently active, controls keybinds maybe
+
+public class PinManager : MonoBehaviour
+{
+    Pin[] pins;
+    string[] keybinds = { "g", "f", "d", "s", "a" };
+
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        pins = FindObjectsOfType<Pin>();
+    }
+    void Start()
+    {
+        // assigns a key to each pin in the game
+        if (keybinds.Length > pins.Length) // no crashy
+        {
+            for (int i = 0; i < pins.Length; i++)
+            {
+                pins[i].keybind = keybinds[i];
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
